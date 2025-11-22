b71da941225f65d3cfe2e4b9c8ebb18b88b0b779
jenicemonteiro7-blip <jenicemonteiro7@gmail.com>
2025-11-22 17:08:02 +0530

    Add files via upload

---
+++ src/AttendanceManager.cpp
@@ -0,0 +1,43 @@
+#include "AttendanceManager.h"
+#include <fstream>
+#include <iostream>
+
+AttendanceManager::AttendanceManager() {}
+
+void AttendanceManager::markAttendance(const Attendance &record) {
+    attendanceList.push_back(record);
+}
+
+float AttendanceManager::getPercentage(std::string studentID, std::string subjectID) {
+    int total = 0, present = 0;
+
+    for (auto &a : attendanceList) {
+        if (a.getStudentID() == studentID && a.getSubjectID() == subjectID) {
+            total++;
+            if (a.getStatus()) present++;
+        }
+    }
+
+    if (total == 0) return 0;
+    return (present * 100.0f) / total;
+}
+
+std::vector<Attendance> AttendanceManager::getAttendanceBySubject(std::string subjectID) {
+    std::vector<Attendance> result;
+
+    for (auto &a : attendanceList) {
+        if (a.getSubjectID() == subjectID) {
+            result.push_back(a);
+        }
+    }
+
+    return result;
+}
+
+void AttendanceManager::loadFromFile() {
+    // TODO: read CSV (attendance.csv)
+}
+
+void AttendanceManager::saveToFile() {
+    // TODO: write to CSV (attendance.csv)
+}
