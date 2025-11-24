d5a1546c1393a3a10c4987941bdaa5f140120eb7
SamanthaCoutinho <samanthacoutinho2005@gmail.com>
2025-11-24 15:23:06 +0530

    Add files via upload

--- gui/dashboardwindow.cpp
+++ gui/dashboardwindow.cpp
@@ -1,6 +1,8 @@
 #include "dashboardwindow.h"
 #include "ui_dashboardwindow.h"
 #include "loginwindow.h"
+#include "attendancewindow.h"
+#include "studentwindow.h"
 #include <QMessageBox>
 
 DashboardWindow::DashboardWindow(QWidget *parent)
@@ -22,15 +24,14 @@ void DashboardWindow::on_logoutButton_clicked()
     this->close();
 }
 
-
 void DashboardWindow::on_attendanceButton_clicked()
 {
-    QMessageBox::information(this, "Manage Attendance", "Attendance screen coming soon.");
+    AttendanceWindow *aw = new AttendanceWindow();
+    aw->show();
 }
-
 
 void DashboardWindow::on_studentsButton_clicked()
 {
-    QMessageBox::information(this, "Manage Students", "Student management screen coming soon.");
+    StudentWindow *sw = new StudentWindow();
+    sw->show();
 }
-
