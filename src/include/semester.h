4dbbb39e211183dd2af67ab179444f527e157403
Ayushi14022005 <msci.2326@unigoa.ac.in>
2025-11-24 12:37:01 +0530

    Push

--- src/include/semester.h
+++ src/include/semester.h
@@ -9,11 +9,10 @@ class Semester {
 private:
     std::string semesterID;
     int semesterNumber;
+    std::string SemesterName;
     std::vector<Subject*> subjects;
-
 public:
     Semester(std::string id, int number);
-
     void addSubject(Subject* sub);
     std::string getSemesterID() const;
     int getSemesterNumber() const;
